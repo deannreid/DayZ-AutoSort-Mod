0b41893cc23ee32661b4248a6e3df10c783e33dd
Dean Reid <deannreid@hotmail.co.uk>
2022-03-10 19:21:10 +0000

    Big Update kinda

---
+++ build/Addons/scripts/3_Game/AutoSorting/enum/ACSPrepConf.c
@@ -0,0 +1,21 @@
+/*
+ * DayZ - Storage Auto Sorter mod.
+ * Developed by @Dean Reid
+ *
+ * Class: acs_config.c
+ *  
+ * Class Information:
+ *
+ * Class handles configuration parameters
+ * 
+ * Program Version: 1.0
+ * Code Version: 1.0
+ * 
+ * Updates: 
+ * 06/03/2022 - Initial Code Development
+ */
+
+enum APCPrepConfig 
+{
+		checkACPConfig
+}
---
+++ build/Addons/scripts/Define.c
@@ -0,0 +1,17 @@
+/*
+ * DayZ - Storage Auto Sorter mod.
+ * Developed by @Dean Reid
+ *
+ * Class: Define.c
+ *  
+ * Class Information:
+ *
+ * Class handles definition of ACS
+ * 
+ * Program Version: 1.0
+ * Code Version: 1.0
+ * 
+ * Updates: 
+ * 06/03/2022 - Initial Code Development
+ */
+#define ACS
